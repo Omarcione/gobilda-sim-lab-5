4cf5319c3e86f579acbe8063a9310c135474984c
Jack Stewart <jack@casastewart.us>
2025-11-15 16:05:11 -0800

    completed predict function and set op rest of program

--- gobilda_sim/ros_ws/src/gobilda_utilities/gobilda_utilities/ekf.py
+++ gobilda_sim/ros_ws/src/gobilda_utilities/gobilda_utilities/ekf.py
@@ -1,9 +1,7 @@
-from enum import Enum
+from collections import deque
+import numpy as np
 import rclpy
 from rclpy.node import Node
-import math
-from geometry_msgs.msg import TwistStamped # type: ignore
-from geometry_msgs.msg import Twist # type: ignore
 from sensor_msgs.msg import Odometry, Imu # type: ignore
             
 class EKFNode(Node):
@@ -13,11 +11,33 @@ class EKFNode(Node):
         # 'ros2 node list'
         super().__init__('ekf_node')
 
-        lidar_buffer = []
-        odom_buffer = []
+        # State variables
+        self.state_vector = np.zeros((3, 1))  # [x, y, theta]
+        self.covariance_matrix = np.eye(3) * 1e-4  # 3x3 Identity matrix as initial covariance
         
-        # Functions running at 10Hz
-        timer_period = 0.1  # seconds
+            # State vector:
+            # [[x],
+            # [y],
+            # [theta]]
+
+            # Initial covariance matrix:
+            # [[0.0001, 0.    , 0.    ],
+            # [0.    , 0.0001, 0.    ],
+            # [0.    , 0.    , 0.0001]]
+
+        # Process noise (Q)
+        # Larger value for theta since IMU drift mainly affects orientation
+        self.Q = np.diag([1e-4, 1e-4, 1e-3])
+
+        # Buffers for incoming data
+        self.imu_queue = deque()    # Predict step
+        self.odom_queue = deque()   # Update step
+
+        # Keep track of last IMU time to compute dt
+        self.last_imu_time = None
+        
+        # Functions running at 20Hz
+        timer_period = 0.05  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
 		# lidar odom subscriber
@@ -26,22 +46,88 @@ class EKFNode(Node):
 		# imu sub
         self.imu_sub = self.create_subscription(Imu,'/oak/camera/imu_data/', self.imu_callback, 10)
 
-    def ekf_predict(self, imu_buffer, odom_buffer):
+    def ekf_step(self):
+        # Predict using last IMU
+        if self.imu_queue:
+            imu_msg = self.imu_queue.pop()
+            self.predict(imu_msg)
+
+        # Update if LiDAR available
+        if self.odom_queue:
+            lidar_msg = self.lidar_queue.pop()
+            self.update(lidar_msg)
+
+        # Publish fused state
+        self.publish_state()
+    
+    def predict(self, imu_msg):
+        """
+        Predict step using IMU angular velocity (yaw rate).
+        Only theta is predicted; x,y remain unchanged and will be corrected by LiDAR.
+        """
+        ## Calculate change in time (dt)
+        current_time = imu_msg.header.stamp.sec + imu_msg.header.stamp.nanosec * 1e-9
+
+        if self.last_imu_time is None:
+            self.last_imu_time = current_time
+            return
+        
+        dt = current_time - self.last_imu_time
+        self.last_imu_time = current_time
+
+        if dt <= 0:
+            return  # No time has passed, skip prediction
+        
+
+        ## Predict theta from angular velocity
+        angular_velocity_z = imu_msg.angular_velocity.z
+        theta = self.state_vector[2, 0]
+
+        theta_prediction = theta + angular_velocity_z * dt
+        self.state_vector[2, 0] = theta_prediction
+
+
+        ## Covariance
+        # Motion model Jacobian F. Since we only predict theta, F is just identity.
+        F = np.eye(3)
+
+        # Scale Q by dt (simple model: more time, more uncertainty)
+        Q_dt = self.Q * dt
+
+        # Update covariance
+        # @ == matrix multiplication
+        # equation: P' = FPF^T + Q
+        self.covariance_matrix = F @ self.covariance_matrix @ F.T + Q_dt
         return
     
-    # Callback for the events
+    def update(self, lidar_msg):
+        """
+        Update step using LiDAR odometry:
+        Measurement z = [x_lidar, y_lidar, theta_lidar]^T
+        """
+        return
+    
+    def publish_state(self):
+        """
+        Publish fused state as an Odometry message.
+        
+        You can use the ros2bag CLI to record the state of the robot after it is published. 
+        Then you can write a python script to load the data and plot it.
+        """
+        return
+    
+    # Callbacks for the events
+    def imu_callback(self, msg):
+        self.imu_buffer.append(msg)
+        return
     
     def odom_callback(self, msg):
-        pos_x, pos_y, pos_z = msg.pose.pose.position
-        
-        self.odom_buffer.append()
+        self.odom_buffer.append(msg)
         return
-	
-    def imu_callback(self, msg):
-      return
     
-    def timer_callback(self):        
-       return
+    def timer_callback(self): 
+        self.ekf_step()       
+        return
 
 
 def main(args=None):
